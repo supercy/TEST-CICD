1f4457f3d60b4179bfd08ec2032f7d2ad360f14d
ethan.cui <Ethan.cui@lcfuturecenter.com>
2020-11-30 19:34:34 +0800

    sfsdfsf

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      666
+      test
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
         >vue-cli documentation</a
       >.
